d407cce8941a005963fdaf1ae478540fa7e84541
Ivan Tkachenko <ivan.tkachenko@starcode.ru>
2016-08-16 14:41:42 +0300

    experiments with react-autocomplete

--- src/app.jsx
+++ src/app.jsx
@@ -1,15 +1,67 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './main.scss';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Autocomplete from 'react-autocomplete'
+import { getStates, matchStateToTerm, sortStates, styles, fakeRequest } from 'react-autocomplete/build/lib/utils'
+import './main.scss'
 
-class Hello extends React.Component {
+class App extends React.Component {
+
+    constructor() {
+        super();
+
+        this.state = {
+            value: '',
+            unitedStates: getStates(),
+            loading: false
+        }
+    }
+
     render() {
         return (
             <div>
-                <p>Hello React!</p>
+                <Autocomplete
+                    inputProps={{name: "US state", className: "input"}}
+                    value={this.state.value}
+                    items={this.state.unitedStates}
+                    getItemValue={(item) => item.name}
+                    onSelect={(value, item) => {
+                        this.setState({ value, unitedStates: [ item ] })
+                    }}
+                    onChange={(event, value) => {
+                        this.setState({ value, loading: true });
+                        fakeRequest(value, (items) => {
+                            this.setState({ unitedStates: items, loading: false })
+                        })
+                    }}
+                    renderItem={(item, isHighlighted) => (
+                        <div
+                            key={item.abbr}
+                            id={item.abbr}
+                            className="custom-class-item"
+                        >{item.name}</div>
+                    )}
+                    renderMenu={(items, value) => (
+                        <div className="autocomplete-items-wrapper">
+                            {value === '' ? (
+                                <div>Type of the name of a United State</div>
+                            ) : this.state.loading ? (
+                                <div>Loading...</div>
+                            ) : items.length === 0 ? (
+                                <div>No matches for {value}</div>
+                            ) : this.renderItems(items)}
+                        </div>
+                    )}
+                />
             </div>
         )
     }
+
+    renderItems (items) {
+        return items.map((item) => {
+            return item
+        })
+    }
+
 }
 
-ReactDOM.render(<Hello />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'));
