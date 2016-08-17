9af4a30767d76b885adef66b975a0e801a2c66ee
Ivan Tkachenko <vanekt@yandex>
2016-08-17 22:21:03 +0300

    layout sketch

--- src/app.jsx
+++ src/app.jsx
@@ -8,9 +8,35 @@ class App extends React.Component {
 
     render() {
         return (
-            <div>
-                <Profession />
-                <Phone />
+            <div className="signup-form-wrapper">
+                <p className="signup-form-text"><strong>Зарегистрируйтесь</strong> и начните продавать услуги через интернет сегодня</p>
+                <form>
+                    <div>
+                        <div className="input-group input-group--inline">
+                            <label>Имя</label>
+                            <input name="firstName" />
+                        </div>
+    
+                        <div className="input-group input-group--inline">
+                            <label>Фамилия</label>
+                            <input name="lastName" />
+                        </div>
+                        
+                        <div className="clear"></div>
+                    </div>
+
+                    <div className="input-group">
+                        <label>Профессия</label>
+                        <Profession />
+                    </div>
+
+                    <div className="input-group">
+                        <label>Телефон</label>
+                        <Phone />
+                    </div>
+
+                    <button>Зарегистрироваться</button>
+                </form>
             </div>
         )
     }
