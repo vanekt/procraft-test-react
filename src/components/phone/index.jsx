1531f59af35956f0b60f2c0b9dac71478ef5b38d
Ivan Tkachenko <ivan.tkachenko@starcode.ru>
2016-08-18 16:52:56 +0300

    invert caret on toggle

--- src/components/phone/index.jsx
+++ src/components/phone/index.jsx
@@ -16,47 +16,48 @@ export class Phone extends React.Component {
 
         this.state = {
             selected: countries[0],
-            focused: false
+            focused: false,
+            opened: false
         };
 
         this.dropDownOnSelectCallback = this.dropDownOnSelectCallback.bind(this);
         this.phoneInputFocus = this.phoneInputFocus.bind(this);
         this.phoneInputBlur = this.phoneInputBlur.bind(this);
         this.renderDropdownList = this.renderDropdownList.bind(this);
+        this.toggleOpenedState = this.toggleOpenedState.bind(this);
     }
 
     render() {
         const dropDownTitle = (
             <div>
                 <img src={"assets/phone/img/" + this.state.selected.img} alt={this.state.selected.name} />
-                <FontAwesome name='angle-down' size="lg" className="custom-caret" />
+                <FontAwesome name={this.state.opened ? 'angle-up' : 'angle-down'} size="lg" className="custom-caret" />
             </div>
         );
 
         return (
-            <div className="phone-input">
-                <FormGroup>
-                    <InputGroup>
-                        <DropdownButton
-                            componentClass={InputGroup.Button}
-                            id="input-dropdown-addon"
-                            title={dropDownTitle}
-                            className={"without-box-shadow phone-dropdown-control" + (this.state.focused ? ' focused' : '')}
-                            noCaret={true}
-                        >
-                            {this.renderDropdownList()}
-                        </DropdownButton>
-                        <InputGroup.Addon className={"without-box-shadow phone-country-prefix" + (this.state.focused ? ' focused' : '')}>
-                            {this.state.selected.phoneCode}
-                        </InputGroup.Addon>
-                        <FormControl onFocus={this.phoneInputFocus}
-                                     onBlur={this.phoneInputBlur}
-                                     className="phone-number-input without-box-shadow"
-                                     type="text"
-                                     placeholder="495 123-45-67" />
-                    </InputGroup>
-                </FormGroup>
-            </div>
+            <FormGroup>
+                <InputGroup>
+                    <DropdownButton
+                        componentClass={InputGroup.Button}
+                        id="input-dropdown-addon"
+                        title={dropDownTitle}
+                        className={"without-box-shadow phone-dropdown-control" + (this.state.focused ? ' focused' : '')}
+                        noCaret={true}
+                        onToggle={this.toggleOpenedState}
+                    >
+                        {this.renderDropdownList()}
+                    </DropdownButton>
+                    <InputGroup.Addon className={"without-box-shadow phone-country-prefix" + (this.state.focused ? ' focused' : '')}>
+                        {this.state.selected.phoneCode}
+                    </InputGroup.Addon>
+                    <FormControl onFocus={this.phoneInputFocus}
+                                 onBlur={this.phoneInputBlur}
+                                 className="phone-number-input without-box-shadow"
+                                 type="text"
+                                 placeholder="495 123-45-67" />
+                </InputGroup>
+            </FormGroup>
         );
     }
 
@@ -90,5 +91,9 @@ export class Phone extends React.Component {
         }
 
         return items;
+    }
+
+    toggleOpenedState() {
+        this.setState({ opened: !this.state.opened });
     }
 }
