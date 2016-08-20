833829ef20ae33d395833465b55d47acffb235a1
Ivan Tkachenko <vanekt@yandex>
2016-08-20 18:52:58 +0300

    minify html

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -21,7 +21,12 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             filename: 'index.html',
-            template: './src/index.html'
+            template: './src/index.html',
+            minify: {
+                collapseWhitespace: true,
+                removeComments: true,
+                removeScriptTypeAttributes: true
+            }
         }),
         new CopyWebpackPlugin([
             { 
